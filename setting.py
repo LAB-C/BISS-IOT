65ff93d3a9a7301a03e5c6154ba7bebb84dd1775
JunhoYeo <32605822+JunhoYeo@users.noreply.github.com>
2018-11-17 21:03:53 +0900

    ADD: setting

--- setting.py
+++ setting.py
@@ -1,11 +1,41 @@
 from klaytn import *
-import os.path, json
+import os.path, requests, json
 
-klay = Klaytn('http://ubuntu.hanukoon.com:8551')
+url = input('[*] Input Firmware-server URL: ')
+klay = Klaytn(url)
 
-if not os.path.isfile('./info.json'):
-    pass
-    
-wallet = klay.newAcc ount('_labc')
-klay.unlockAccount(wallet, '_labc', 30000)
-print(klay.sendData(wallet, 'test1 by h4nuko0n'))
+def make_info():
+    wallet = klay.newAccount('_labc')
+    info = {
+        'device': {
+            'name': input('[*] Input device name: '),
+            'wallet': wallet
+        },
+        'klaytn-node': input('[*] Input Klaytn node URL: '),
+        'firmware-server': url
+    }
+    klay.unlockAccount(wallet, '_labc', 30000)
+    with open('./info.json', 'w') as f:
+        json.dump(info, f, indent=4)
+    return info
+
+def report_info(info):
+    # 1. Check if same wallet exists in server
+    r = requests.post(url, data={'wallet': info['device']['wallet']})
+    if json.loads(r.text)['message'] == False: # does not exist(new)
+        # 2. Report device information
+        r = requests.post(url, data=info['device'])
+        if 'Success' in r.text:
+            return True
+    return False
+
+if not os.path.isfile('./info.json'): # 존재 x
+    info = make_info()
+    report_info(info)
+else:
+    try:
+        with open('./info.json', 'r') as f:
+            info = json.load(f)
+        report_info(info)
+    except:
+        pass
