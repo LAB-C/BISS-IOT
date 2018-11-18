b119e099c62ce107f75262838e6ed3b24428df05
cokia <hwymaster01@gmail.com>
2018-11-18 21:37:38 +0900

    FIX: install.sh (install caver-js part)

--- install.sh
+++ install.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 # install.sh for client(rpi)
 apt-get --yes install python3
-apt-get --yes install nodejs # install node.js
+curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
+sudo apt-get install -y nodejs #install nodejs
 apt-get --yes install npm # install npm
 
-npm install -g caver-js
+npm install caver-js #if it is not working, how about try -g option?
 
 apt-get --yes install arduino
 pip install arduino-sketch
