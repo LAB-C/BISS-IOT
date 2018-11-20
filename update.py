01d0452ef04d4e5aaa79bee2ef8fae1c3fee5ac1
JunhoYeo <32605822+JunhoYeo@users.noreply.github.com>
2018-11-21 06:51:19 +0900

    FIX: update.py

--- update.py
+++ update.py
@@ -1,7 +1,7 @@
 from client.klaytn import Klaytn
 from client.utils import *
 import urllib.request
-import json, requests
+import json, requests, subprocess
 
 import logging
 logging.basicConfig(level=logging.DEBUG)
@@ -29,30 +29,43 @@ print(file_url)
 file_name = file_url[file_url.rfind("/")+1:]
 print(file_name)
 
+# download file
+urllib.request.urlretrieve(file_url, './firms/' + file_name)
+
+# get file checksum
+file_hash = hash_file('./firms/' + file_name)
+
+# check hash in blockchain
+output = subprocess.Popen(['node', 'client/send.js', 'hash', str(file_id), file_hash], stdout=subprocess.PIPE ).communicate()[0]
+result = json.loads(output.strip().decode())['result']
+
 try:
-    # download file
-    urllib.request.urlretrieve(file_url, './firms/' + file_name)
-
-    # get file checksum
-    file_hash = hash_file('./firms/' + file_name)
-
-    # check hash in blockchain
-    output = subprocess.Popen(['node', 'firmware_server/send.js', 'hash', file_id, file_hash], stdout=subprocess.PIPE ).communicate()[0]
-    result = json.loads(output.strip().decode())['result']
-
     if result:
         print('[*] Success: File Equal! (local-blockchain)')
+
+        # check hash(reporting)
+        res = requests.post(info['firmware_server'] + '/api/check/hash/' + str(file_id), json={
+            'hash': file_hash, 
+            'wallet': info['device']['wallet']
+        })
+        print(res.text)
+        data = json.loads(res.text)
+
+        if data['equal']:
+            print('[*] Success: File Equal! (local-server)')
         
-        # upload
-        if get_ext('./firms/' + file_name) == '.ino':
-            print('[*] Success: Firmware file is .ino file!')
-            upload_device('./firms/' + file_name)
-        
+            # upload to arduino
+            if get_ext('./firms/' + file_name) == '.ino':
+                print('[*] Success: Firmware file is .ino file!')
+                upload_device('./firms/' + file_name)
+            
+            else:
+                print('[*] Error: Firmware file is NOT .ino file!')
         else:
-            print('[*] Error: Firmware file is NOT .ino file!')
+            print('[*] Error: File is Different! (local-server)')
     else:
         print('[*] Error: File is Different! (local-blockchain)')  
 except:
-    pass
+    print('[*] Error while executing')
 
 print('[*] Finish')
